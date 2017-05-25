27e7b35b56da0d908e49dc810af5de450fce8074
Yutaro M <utaro2277@yahoo.co.jp>
2017-05-25 12:54:52 +0900

    Update test.c

--- test.c
+++ test.c
@@ -2,14 +2,14 @@
 int main(void){
   int i, j, n;
   int data[];
-  char name[10][10];
+  char name_stk[10], name[10][10];
   
   printf("人数=");
   scanf("%d", &n);
   
   for(i=0; i<n; i++){
     scanf("%d", &data[i]);
-    scanf("%s", name[i]);
-    strcpy();
+    scanf("%s", name_stk);
+    strcpy(name[i], name_stk);
   }
 }
