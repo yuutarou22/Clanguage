c2cad32522147ecbc43a0c581aceeb0b1ed0bfd2
Yutaro M <utaro2277@yahoo.co.jp>
2017-05-25 12:52:54 +0900

    Update and rename test.txt to test.c

---
+++ test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void){
+  int i, j, n;
+  int data[];
+  char name[10][10];
+  
+  printf("人数=");
+  scanf("%d", &n);
+  
+  for(i=0; i<n; i++){
+    scanf("%d", &data[i]);
+    scanf("%s", name[i]);
+    strcpy();
+  }
+}
